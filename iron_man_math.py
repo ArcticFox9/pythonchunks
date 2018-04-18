ae566930decfd792f18de7a65922a84c225a9b2d
ArcticFox9 <35636995+ArcticFox9@users.noreply.github.com>
2018-04-17 19:01:10 -0700

    IRON MAN ROCKS!!!!!!!!!!!!!!!!!!!

---
+++ iron_man_math.py
@@ -0,0 +1,14 @@
+# Iron Man Math
+import random
+
+
+print("I AM IRON MAN")
+print("I KICK BUTTS!!!!!")
+print("LOTS OF BUTTS!!!!!")
+print("I AM AWESOME!!!!!")
+
+print("Today I kicked")
+N = random.randint(50,77)
+
+print(N)
+print("butts")
