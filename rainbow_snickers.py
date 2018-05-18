d2071472e3ab6e50dfb317c497c9a64786c335a1
snowLeapord9 <35636995+ArcticFox9@users.noreply.github.com>
2018-05-18 15:52:27 -0700

    hah!

---
+++ rainbow_snickers.py
@@ -0,0 +1,26 @@
+#count=input("how many would you like?")
+count = int(input("Enter a number: "))
+
+while (count > 0):
+    print("meow meow")
+    print("pow pow pow")
+    print("supercat!!!")
+    count=count-1
+
+
+#count=input("how many w`ould you like?")
+count = int(input("Enter another number: "))
+
+while (count > 0):
+    print("I fight crime,Kick butt,and")
+    count=count-1
+
+
+count = int(input("Enter a number:"))
+
+while (count > 0):
+    print("Drink")
+    print("Grapefruit")
+    print("JUICE!")
+    print("(my sourse of powah)")
+    count=count-1
