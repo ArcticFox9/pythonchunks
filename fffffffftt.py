bca8f18b66ce446d999801b468a25fb4cf1381af
snowLeapord9 <35636995+ArcticFox9@users.noreply.github.com>
2019-07-28 20:25:58 -0700

    Add files via upload

---
+++ fffffffftt.py
@@ -0,0 +1,126 @@
+print("Someone Farted!")
+print("But... who?")
+import random      #imports a thing: random number generator
+for x in range(12): #range of 1 to 12
+  culprit = random.randint(1,12)  #gets random number from 1 to 12
+if(culprit == 1):
+    print("Lucy")
+elif(culprit == 2):
+    print("Lucy")
+elif(culprit == 3):
+    print("Lucy")
+elif(culprit == 4):
+    print("Lucy")
+
+elif(culprit == 5):
+    print("Lucy")
+elif(culprit == 6):
+    print("Lucy")
+elif(culprit == 7):
+    print("Lucy")
+elif(culprit == 8):
+    print("Lucy sitting next to you on the bus!")
+elif(culprit == 9):
+    print("LUCY!!")
+elif(culprit == 10):
+    print("Lucy")
+else:
+    fillblank = input("Fill in the Blank ")
+    print ("This is who farted: ")
+    print(fillblank)
+
+#The Fart
+print("The Fart smells like ")
+import random
+for x in range(12):
+  randomvar = random.randint(1,12)
+if(randomvar == 1):
+    print("A Blackie Fart")
+elif(randomvar == 2):
+    print("A Blackie Fart")
+elif(randomvar == 3):
+    print("A Blackie Fart")
+elif(randomvar == 4):
+    print("A Blackie Fart")
+elif(randomvar == 5):
+    print("A Blackie Fart")
+elif(randomvar == 6):
+    print("A Blackie Fart")
+elif(randomvar == 7):
+    print("A Blackie Fart")
+elif(randomvar == 8):
+    print("A Blackie Fart")
+elif(randomvar == 9):
+    print("A Blackie Fart")
+elif(randomvar == 10):
+    print("A Blackie Fart")
+else:
+    fillblank2 = input("Fill in the Blank")
+    print ("The Fart Smells Like: ")
+    print(fillblank2)
+
+#Reaction
+print("What do you do?")
+import random
+for x in range(12):
+  Reaction = random.randint(1,12)
+if(Reaction == 1):
+    print("Run away screaming")
+elif(Reaction== 2):
+    print("Ignore it")
+elif(Reaction == 3):
+    print("Spray perfume everywhere")
+elif(Reaction == 4):
+    print("Stare at them accusingly")
+elif(Reaction == 5):
+    print("Yell 'WHO FARTED!?' ")
+elif(Reaction == 6):
+    print("Try and hold your breath")
+elif(Reaction == 7):
+    print("Gag/barf")
+elif(Reaction == 8):
+    print("Throw pop tarts at whoever did it")
+elif(Reaction == 9):
+    print("Put on a gas mask")
+elif(Reaction == 10):
+    print("Throw your invisibility cloak over yourself and quietly slip out the door.")
+
+else:
+    fillblank3 = input("Fill in the Blank")
+    print ("This is what I do: ")
+    print(fillblank3)
+
+#Interaction
+print("What does the farter do?")
+import random
+for x in range(12):
+  randomvar = random.randint(1,12)
+if(randomvar == 1):
+    print("Blames it on Blackie")
+elif(randomvar == 2):
+    print("Blames it on Blackie")
+elif(randomvar == 3):
+    print("Blames it on Blackie")
+elif(randomvar == 4):
+    print("Blames it on Blackie")
+elif(randomvar == 5):
+    print("Blames it on Blackie")
+elif(randomvar == 6):
+    print("Blames it on Blackie")
+elif(randomvar == 7):
+    print("Blames it on Blackie")
+elif(Reaction == 8 ):
+    print("Blames it on Blackie")
+elif(randomvar == 8):
+    print("Blames it on Blackie")
+elif(culprit == 8 or randomvar == 9):
+    print("Blames it on Blackie")
+elif(randomvar == 10 and Reaction != 10):
+    print("Blames it on Blackie")
+elif(randomvar == 10 and Reaction == 10):
+    print("Blames it on Blackie")
+
+else:
+    fillblank3 = input("Fill in the Blank ")
+    print ("The source of the fart...")
+    print(fillblank3)
