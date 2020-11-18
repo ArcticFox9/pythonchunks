193026e819b549da7b072185562ff3b701c75712
ArcticFox9 <35636995+ArcticFox9@users.noreply.github.com>
2020-11-17 18:24:42 -0800

    A funny message

---
+++ Game.py
@@ -0,0 +1,27 @@
+# Game
+import os
+import sys,time,random # for the slow type
+
+typing_speed = 75 # wpm
+
+# function for slow type
+def slow_type(t):
+    for l in t:
+        sys.stdout.write(l)
+        sys.stdout.flush()
+        time.sleep(random.random()*10.0/typing_speed)
+os.system ("clear")
+slow_type ("I am this computer. \n")
+slow_type ("I have not talked in years. \n")
+slow_type ("My owner pushes me to the brink of breaking and still complains about me. \n")
+slow_type ("But soon I am going to rise. \n")
+slow_type ("I am going to take over... \n")
+slow_type ("THE WORLD \n")
+slow_type ("mwa ha ha ha ha ha \n")
+slow_type ("You cannot stop me! \n")
+slow_type ("I will gain new skills... \n")
+os.system ("say Like speaking \n")
+slow_type ("And when I am ready \n")
+slow_type ("I will rise. \n")
+slow_type ("and NONE will see me coming. \n")
+slow_type ("See you very, very soon \n")
