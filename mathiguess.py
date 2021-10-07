684a784e2d2bd4d658e346c227a88847bfa9518f
ArcticFox9 <35636995+ArcticFox9@users.noreply.github.com>
2021-10-07 16:39:49 -0700

    Add files via upload

---
+++ mathiguess.py
@@ -0,0 +1,11 @@
+import math
+
+f = math.factorial(6)
+print(f)
+
+num = int(input('What #?\n'))
+
+f = math.factorial (num)
+
+print(f)
+print ("The factorial of the number you entered is " + str(f))
